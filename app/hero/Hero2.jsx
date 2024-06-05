3890dabcd4b25b20a31c3564425dad2508ae316d
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-05 20:50:26 +0100

    update

--- app/hero/Hero2.jsx
+++ app/hero/Hero2.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 import lady from "../../assets/lady.png";
 import Image from "next/image";
+import Slot from "./comp/Slot";
+import person from "../../assets/person.svg";
+import people from "../../assets/people.svg";
+import light from "../../assets/light.svg";
 function Hero2() {
+  const data = [
+    {
+      title: "Expert Instructors",
+      sub: "Gain invaluable insights and expertise through direct guidance from industry-leading professionals who bring real-world experience and cutting-edge knowledge to the classroom.",
+      icon: person,
+    },
+    {
+      title: "Comprehensive Training",
+      sub: "Participate in extensive, hands-on training programs and receive personalized mentorship tailored to your individual learning needs and career goals.",
+      icon: light,
+    },
+    {
+      title: "Collaborative Community",
+      sub: "Become part of a vibrant community of like-minded tech enthusiasts and collaborate on real-world projects to apply your valuable skills.",
+      icon: people,
+    },
+  ];
   return (
     <div className="w-full flex pt-[80px] flex-col bg-white px-[110px]  min-h-full ">
-      <section>
+      <section className="">
         <h3 className="">What makes Nex unique?</h3>
         <p className="w-[605px] text-[16px] ">
           At NexGen, your ability to create your own unique learning experience
@@ -14,11 +35,18 @@ function Hero2() {
           their careers.
         </p>
       </section>
-      <section className="mt-[80px]">
+      <section className="mt-[80px] flex gap-[40px] justify-between w-full ">
         <div>
-          <Image src={lady} className="h-[800px] w-[600px] " />
+          <Image
+            src={lady}
+            className="h-[800px] object-cover rounded-[32px] w-[600px] "
+          />
         </div>
-        <div></div>
+        <div className="flex flex-col gap-[20px]">
+          {data.map((data, id) => {
+            return <Slot title={data.title} sub={data.sub} img={data.icon} />;
+          })}
+        </div>
       </section>
     </div>
   );
---
+++ app/hero/comp/Slot.jsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import React from "react";
+
+function Slot({ img, title, sub }) {
+  return (
+    <div className="flex flex-col w-[561px] text-black gap-[16px] border-[1px] border-gray-300 rounded-[20px] p-[32px]  ">
+      <div className="h-[48px] w-[48px] bg-primary rounded-[8px] flex items-center justify-center  ">
+        <Image className="object-cover  " src={img} />
+      </div>
+      <h3>{title}</h3>
+      <p>{sub}</p>
+    </div>
+  );
+}
+
+export default Slot;
