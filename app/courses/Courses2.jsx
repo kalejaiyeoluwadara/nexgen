21f53d59f2a19b211abec02c4a588e1065b1b1dd
kalejaiyeoluwadara <kalejaiyeoluwadara1@gmail.com>
2024-06-07 10:17:09 +0100

    updates

--- app/courses/Courses2.jsx
+++ app/courses/Courses2.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import lady from "../../assets/lady.png";
+import man from "../../assets/man.png";
 import Image from "next/image";
 import person from "../../assets/person.svg";
 import people from "../../assets/people.svg";
@@ -8,34 +8,29 @@ import Slot from "../hero/comp/Slot";
 function Courses2() {
   const data = [
     {
-      title: "Expert Instructors",
-      sub: "Gain invaluable insights and expertise through direct guidance from industry-leading professionals who bring real-world experience and cutting-edge knowledge to the classroom.",
+      title: "Part-Time Learning",
+      sub: "Learning doesn’t have to mean putting your life on hold. We’ve designed our courses, certificates, and degrees to give you the flexibility to pace your studies to fit your schedule.",
       icon: person,
     },
     {
-      title: "Comprehensive Training",
-      sub: "Participate in extensive, hands-on training programs and receive personalized mentorship tailored to your individual learning needs and career goals.",
-      icon: light,
-    },
-    {
-      title: "Collaborative Community",
-      sub: "Become part of a vibrant community of like-minded tech enthusiasts and collaborate on real-world projects to apply your valuable skills.",
-      icon: people,
+      title: "Flexible Schedules",
+      sub: "We offer courses multiple times a year, in terms of varying lengths — so you can balance school, career, and other life commitments.",
+      icon: person,
     },
   ];
   return (
     <main className="bg-white">
       <section className="mt-[80px] flex gap-[40px] justify-between w-full ">
-        <div>
-          <Image
-            src={lady}
-            className="h-[800px] object-cover rounded-[32px] w-[600px] "
-          />
-        </div>
         <div className="flex flex-col gap-[20px]">
           {data.map((data, id) => {
             return <Slot title={data.title} sub={data.sub} img={data.icon} />;
           })}
+        </div>
+        <div>
+          <Image
+            src={man}
+            className="h-[800px] object-cover rounded-[32px] w-[600px] "
+          />
         </div>
       </section>
     </main>
